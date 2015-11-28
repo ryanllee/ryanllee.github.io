82b1ba581dcc1d36e5a700044add8a670eb82e92
Ryan Lee <ryanllee@berkeley.edu>
2015-11-28 15:22:45 -0800

    more javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
   var previousLink;
+  var tracker=1;
 
   $("#me img").hover(function() {
       previousLink = $(this).attr("src");
       $(this).attr("src", "https://pbs.twimg.com/profile_images/592787564106506241/OcuzEuXQ.jpg");
       }, function() {
         $(this).attr("src", previousLink);
+    });
+
+  $("#me img").click(function() {
+    if(tracker == 1) {
+      $("#background").attr("src", "assets/img/school1.jpg");
+      tracker = 2;
+    } else if(tracker == 2) {
+      $("#background").attr("src", "assets/img/school2.jpg");
+      tracker = 3;
+    } else if(tracker == 3) {
+      $("#background").attr("src", "assets/img/school3.jpg");
+      tracker = 4;
+    } else if(tracker == 4) {
+      $("#background").attr("src", "assets/img/SatherGate.jpg");
+      tracker = 1;
     }
-    );
+  });
 
   $("#header").click(function() {
     $("#test1").css("display", "block")
@@ -27,7 +43,6 @@ $(document).ready(function() {
     $("#test4 img").css("display", "block")
               .fadeIn("slow");
   });
-
 
   $("#contentframe").click(function() {
     $("#test1").css("display", "none")
